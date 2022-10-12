5f150d303b19e4a9b9feca6603c2398d8b8107db
DKSHS <88355080+ShadowsS01@users.noreply.github.com>
2022-10-12 20:37:54 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./styles/global.css";
+
+export function App() {
+  return (
+    <>
+      <h1 className="text-cyan-500 text-2xl font-bold mt-2">Hello World</h1>
+
+      <button className="bg-cyan-500 p-2 rounded text-gray-900 font-medium hover:bg-cyan-300 duration-300">
+        Enviar
+      </button>
+    </>
+  );
+}
